b4a6539386d7d9baea5297f48335047627c59612
wow211-js <83398370+wow211-js@users.noreply.github.com>
2026-06-12 05:36:39 +0500

    Add Cloudflare Tunnel: update build.sh

--- build.sh
+++ build.sh
@@ -3,3 +3,9 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+
+# Download cloudflared binary
+CLOUDFLARED_VERSION="2025.4.0"
+curl -fsSL "https://github.com/cloudflare/cloudflared/releases/download/${CLOUDFLARED_VERSION}/cloudflared-linux-amd64" -o cloudflared
+chmod +x cloudflared
+mv cloudflared /opt/render/project/bin/cloudflared
