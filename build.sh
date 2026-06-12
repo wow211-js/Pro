d21d18829c8d0177357480aefbf221130522a16a
wow211-js <83398370+wow211-js@users.noreply.github.com>
2026-06-12 05:58:24 +0500

    Fix build.sh: keep cloudflared in project root

--- build.sh
+++ build.sh
@@ -4,8 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
-# Download cloudflared binary
+# Download cloudflared binary into project directory
 CLOUDFLARED_VERSION="2025.4.0"
 curl -fsSL "https://github.com/cloudflare/cloudflared/releases/download/${CLOUDFLARED_VERSION}/cloudflared-linux-amd64" -o cloudflared
 chmod +x cloudflared
-mv cloudflared /opt/render/project/bin/cloudflared
