6e19e92500b632d9dadef9b6c2f8e1b16472b726
wow211-js <83398370+wow211-js@users.noreply.github.com>
2026-06-13 05:44:21 +0300

    Remove VisitorTrackingMiddleware from settings

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'accounts.middleware.VisitorTrackingMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
