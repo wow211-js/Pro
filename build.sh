afc3cbd790e07d65b55d517d73010a89a6b15ecd
wow211-js <83398370+wow211-js@users.noreply.github.com>
2026-06-16 14:12:58 +0300

    Download openpgp.min.js into static during build

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+
+# Download OpenPGP.js into static files (avoid CDN dependency)
+curl -fsSL "https://cdn.jsdelivr.net/npm/openpgp@5.11.2/dist/openpgp.min.js" -o static/js/openpgp.min.js
