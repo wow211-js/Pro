86a2c1f60ee46f0762ffcb3dc671e425b388ce7d
wow211-js <83398370+wow211-js@users.noreply.github.com>
2026-06-16 14:03:11 +0300

    Override login with CustomLoginView

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from accounts.views import CustomLoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('accounts/login/', CustomLoginView.as_view(), name='login'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
