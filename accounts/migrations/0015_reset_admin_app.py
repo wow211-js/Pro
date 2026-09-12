32239e638c66ae3416647e2aa2b17b161bfe2a51
wow211-js <83398370+wow211-js@users.noreply.github.com>
2026-09-12 12:07:58 +0000

    Update 0015_reset_admin_app.py

--- accounts/migrations/0015_reset_admin_app.py
+++ accounts/migrations/0015_reset_admin_app.py
@@ -18,7 +18,7 @@ def reverse_noop(apps, schema_editor):
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('yourapp', '0014_callsignal'),  
+        ('accounts', '0014_callsignal'),  
     ]
     operations = [
         migrations.RunPython(create_admin, reverse_noop),
